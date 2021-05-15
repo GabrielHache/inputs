2476398c3927fa4dafb83194899732ee2354412b
GabrielHache <80431306+GabrielHache@users.noreply.github.com>
2021-05-15 15:56:33 -0400

    Add files via upload

---
+++ tomanocu/cadastro.js
@@ -0,0 +1,26 @@
+
+ $("#bt_R").click(
+    function(){ 
+      if   
+    (document.getElementById ('cadastro_email').value == '')
+     alert('Favor preencher seu email')
+     else if
+    (document.getElementById ('cadastro_senha').value == '')
+       alert('Favor preencher sua senha')
+       else 
+       if
+       (document.getElementById ('cadastro_nome').value == '')
+          alert('Favor preencher seu nome')
+          else 
+          if
+          (document.getElementById ('cadastro_fone').value == '')
+          alert('Favor preencher seu numero de telefone')
+   
+     else{
+      alert("Registro feito com sucesso! ")
+    }
+    
+  
+      
+      
+    }) 
---
+++ tomanocu/index.js
@@ -0,0 +1,20 @@
+
+ $("#bt_l").click(
+  function(){ 
+    if   
+  (document.getElementById ('input_email').value == '')
+   alert('Favor preencher seu email')
+   else if
+  (document.getElementById ('input_senha').value == '')
+     alert('Favor preencher sua senha')
+ 
+   else{
+    alert("entrando")
+  }
+  
+
+    
+    
+  }) 
+ 
+
